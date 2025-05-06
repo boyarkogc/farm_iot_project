c4fa5b7d1672567b19bc2a36a838705ab554aa52
boyarkogc <boyarko.greg@gmail.com>
2025-05-06 14:37:02 -0600

    changed to not use firebase emulators

--- react-frontend/src/components/dashboard-chart.tsx
+++ react-frontend/src/components/dashboard-chart.tsx
@@ -115,9 +115,7 @@ export default function DashboardChart() {
                 className="flex flex-1 flex-col justify-center gap-1 border-t px-6 py-4 text-left even:border-l data-[active=true]:bg-muted/50 sm:border-l sm:border-t-0 sm:px-8 sm:py-6"
                 onClick={() => setActiveChart(chart)}
               >
-                <span className="text-xs text-muted-foreground">
-                  {activeConfig[chart].label}
-                </span>
+                <span className="text-xs text-muted-foreground">{key}</span>
               </button>
             );
           })}
